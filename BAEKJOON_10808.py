bd2e82fefff96660857a7a69d5ee1dbaf22a16ce
d0bee <gusdud9745@gmail.com>
2023-12-02 21:38:11 +0900

    10808

---
+++ BAEKJOON_10808.py
@@ -0,0 +1,4 @@
+S = input()
+data_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for i in (data_list):
+    print(S.count(i), end=" ")
