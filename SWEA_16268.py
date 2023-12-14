f40a1bf025e12efffcac82bdca060c0664df1a39
d0bee <140006827+d0bee@users.noreply.github.com>
2023-12-14 10:31:24 +0000

    16268

--- SWEA_16268.py
+++ SWEA_16268.py
@@ -1,9 +1,28 @@
 T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split())
-    data = [[0] * M for _ in range(N)]
+# 상 오 하 좌
+dr = [-1,0,1,0]
+dc = [0,1,0,-1]
+d = 0
 
+for _ in range(T):
+    N, M = map(int,input().split())
+
+    data = [[0] * M for _ in range(N)]
     for i in range(N):
         data[i] = list(map(int, input().split()))
     
-    
+    max_list = [[0] * M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            max_list[i][j] += data[i][j]
+            
+            for d in range(4):
+                nr, nc = i + dr[d], j + dc[d]
+                if nr >= N or nr < 0  or nc >= M or nc < 0:
+                    d += 1
+                    continue
+                else:
+                    max_list[i][j] += data[nr][nc]
+                    d += 1
+    for i in max_list:
+        print(i)
